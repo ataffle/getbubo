057e6031d2d0363da4fd7fe4b9a7cf17850c167c
Alix Taffle <ataffle@gmail.com>
2018-12-03 19:13:37 +0100

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Organization.destroy_all
+User.destroy_all
+Commitment.destroy_all
+
+Organization.create(name: "Clickngoat")
+User.create(first_name: "Fred", last_name: "Delacompta", email: "fred@clickngoat.com", organization_id: 1, password: "123456")
