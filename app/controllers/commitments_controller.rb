a1238cc70992a9633ae8dcdee94f9fa5dac24a84
Benjamin Darcet <b.darcet@gmail.com>
2018-12-03 17:02:14 +0100

    commitments creation done

--- app/controllers/commitments_controller.rb
+++ app/controllers/commitments_controller.rb
@@ -1,2 +1,22 @@
 class CommitmentsController < ApplicationController
+  def new
+    @commitment = Commitment.new
+    @suppliers = Supplier.all
+  end
+
+  def create
+    @commitment = Commitment.new(player_params)
+    @commitment.user = current_user
+    if @commitment.save
+      redirect_to commitment_path(@commitment)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def player_params
+    params.require(:commitment).permit(:title, :amount, :description, :due_date, :payment_date, :status, :recurrence, :supplier_id, :retrieval_mode, :payment_method)
+  end
 end
