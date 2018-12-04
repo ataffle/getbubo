e8de4e808b1fca909061581caf81a10fcca26182
Alix Taffle <ataffle@gmail.com>
2018-12-04 17:28:22 +0100

    add table with Datatables plugin

---
+++ app/javascript/components/datatables.js
@@ -0,0 +1,5 @@
+import $ from 'jquery';
+import 'datatables'
+
+$(".table").dataTable();
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,8 @@ import 'select2/dist/css/select2.css';
 import { initSelect2 } from '../components/init_select2';
 import { modalSupplier } from '../components/init_modal';
 import "../components/init_flatpickr"
+import "../components/datatables"
 
 initSelect2();
 modalSupplier();
+
