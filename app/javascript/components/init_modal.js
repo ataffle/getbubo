a7d0d63ca29af9acc74f662057a4775bcb318b17
Benjamin Darcet <b.darcet@gmail.com>
2018-12-07 11:19:46 +0100

    update modal style

--- app/javascript/components/init_modal.js
+++ app/javascript/components/init_modal.js
@@ -3,7 +3,9 @@ import $ from 'jquery';
 const modalSupplier = () => {
   $('#myModal').on('shown.bs.modal', function () {
    $('#myInput').focus()
+   $('span.select2-dropdown--below').addClass("dropdown-inactive");
   })
+  //$('span.select2-dropdown--below').removeClass("dropdown-inactive");
 }
 
 export { modalSupplier };
