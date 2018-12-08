9cf0f430f6d403dcfc293f8935ae9dab8efd4fd1
Benjamin Darcet <b.darcet@gmail.com>
2018-12-08 15:18:27 +0100

    active test

--- app/javascript/components/init_active.js
+++ app/javascript/components/init_active.js
@@ -1,1 +1,7 @@
 import $ from 'jquery';
+
+$(function(){
+  $('.nav-item').click(function(){
+      $(this).parent().addClass('active').siblings().removeClass('active')
+  })
+})
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,4 +1,21 @@
 class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
+  make_private
+  # def filename
+  #   "#{model.invoice_ref}" if original_filename.present?
+  # end
 
+  def filename
+    if original_filename
+      if model && model.read_attribute(mounted_as).present?
+        model.read_attribute(mounted_as)
+      else
+        @name = "#{model.invoice_ref}"
+      end
+    end
+  end
+
+  def public_id
+    return model.invoice_ref
+  end
 end
