c3e883daf6ffca86c287ea84b217491f86a90619
Benjamin Darcet <b.darcet@gmail.com>
2018-12-10 08:50:55 +0100

    download zip

--- app/javascript/components/datatables.js
+++ app/javascript/components/datatables.js
@@ -5,7 +5,16 @@ import 'datatables'
 $("#table-commitment").dataTable( {
   language: {
     search: "<i class='fas fa-search'></i>",
-    searchPlaceholder: "ex : en attente "
+    searchPlaceholder: "ex : en attente ",
+    lengthMenu: "Afficher _MENU_ en-cours page page",
+    zeroRecords: "Pas d'en-cours",
+    info: "Page _PAGE_ sur _PAGES_",
+    infoEmpty: "No records available",
+    infoFiltered: "(filtré depuis _MAX_ en-cours)",
+    paginate: {
+      previous: "<i class='fas fa-chevron-left'></i>",
+      next: "<i class='fas fa-chevron-right'></i>"
+    }
   },
   "dom": '<"top"f>rt<"bottom"lip><"clear">'
 });
