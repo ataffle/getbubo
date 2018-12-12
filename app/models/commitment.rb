26e2209cda8239f01bb54e9c3b199623df42b957
Benjamin Darcet <b.darcet@gmail.com>
2018-12-12 12:43:21 +0100

    fix seed

--- app/models/commitment.rb
+++ app/models/commitment.rb
@@ -18,25 +18,24 @@ class Commitment < ApplicationRecord
 
   mount_uploader :invoice, PhotoUploader
 
+  scope :one_month_ago, -> { where("due_date > ? AND due_date < ?", 1.month.ago.beginning_of_month, 1.month.ago.end_of_month) }
+  scope :two_months_ago, -> { where("due_date > ? AND due_date < ?", 2.month.ago.beginning_of_month, 2.month.ago.end_of_month) }
+  scope :three_months_ago, -> { where("due_date > ? AND due_date < ?", 3.month.ago.beginning_of_month, 3.month.ago.end_of_month) }
+  scope :four_months_ago, -> { where("due_date > ? AND due_date < ?", 4.month.ago.beginning_of_month, 4.month.ago.end_of_month) }
+  scope :five_months_ago, -> { where("due_date > ? AND due_date < ?", 5.month.ago.beginning_of_month, 5.month.ago.end_of_month) }
 
-  scope :two_months_ago, -> { where("due_date >= ? AND due_date <= ?", 2.month.ago.beginning_of_month, 2.month.ago.end_of_month) }
-  scope :three_months_ago, -> { where("due_date >= ? AND due_date <= ?", 3.month.ago.beginning_of_month, 3.month.ago.end_of_month) }
-  scope :four_months_ago, -> { where("due_date >= ? AND due_date <= ?", 4.month.ago.beginning_of_month, 4.month.ago.end_of_month) }
-  scope :five_months_ago, -> { where("due_date >= ? AND due_date <= ?", 5.month.ago.beginning_of_month, 5.month.ago.end_of_month) }
-  scope :six_months_ago, -> { where("due_date >= ? AND due_date <= ?", 6.month.ago.beginning_of_month, 6.month.ago.end_of_month) }
-
-  scope :two_months_from_now, -> { where("due_date >= ? AND due_date <= ?", 2.month.from_now.beginning_of_month, 2.month.from_now.end_of_month) }
-  scope :three_months_from_now, -> { where("due_date >= ? AND due_date <= ?", 3.month.from_now.beginning_of_month, 3.month.from_now.end_of_month) }
-  scope :four_months_from_now, -> { where("due_date >= ? AND due_date <= ?", 4.month.from_now.beginning_of_month, 4.month.from_now.end_of_month) }
-  scope :five_months_from_now, -> { where("due_date >= ? AND due_date <= ?", 5.month.from_now.beginning_of_month, 5.month.from_now.end_of_month) }
-  scope :six_months_from_now, -> { where("due_date >= ? AND due_date <= ?", 6.month.from_now.beginning_of_month, 6.month.from_now.end_of_month) }
+  scope :one_month_from_now, -> { where("due_date > ? AND due_date < ?", 1.month.from_now.beginning_of_month, 1.month.from_now.end_of_month) }
+  scope :two_months_from_now, -> { where("due_date > ? AND due_date < ?", 2.month.from_now.beginning_of_month, 2.month.from_now.end_of_month) }
+  scope :three_months_from_now, -> { where("due_date > ? AND due_date < ?", 3.month.from_now.beginning_of_month, 3.month.from_now.end_of_month) }
+  scope :four_months_from_now, -> { where("due_date > ? AND due_date < ?", 4.month.from_now.beginning_of_month, 4.month.from_now.end_of_month) }
+  scope :five_months_from_now, -> { where("due_date > ? AND due_date < ?", 5.month.from_now.beginning_of_month, 5.month.from_now.end_of_month) }
 
 
-  scope :current_month, ->(offset = 0) { where("due_date >= ? AND due_date <= ?", Time.now.beginning_of_month + offset.month, Time.now.end_of_month + offset.month) }
-  scope :previous_month, ->(offset = 0) { where("due_date >= ? AND due_date <= ?", Time.now.prev_month.beginning_of_month + offset.month, Time.now.prev_month.end_of_month + offset.month) }
+  scope :current_month, ->(offset = 0) { where("due_date > ? AND due_date < ?", Time.now.beginning_of_month + offset.month, Time.now.end_of_month + offset.month) }
+  scope :previous_month, ->(offset = 0) { where("due_date > ? AND due_date < ?", Time.now.prev_month.beginning_of_month + offset.month, Time.now.prev_month.end_of_month + offset.month) }
 
-  scope :next_month, -> { where("due_date >= ? AND due_date <= ?", Time.now.next_month.beginning_of_month, Time.now.next_month.end_of_month) }
-  scope :year_to_date, -> { where("due_date >= ? AND due_date <= ?", Time.now.beginning_of_year, Time.now) }
+  scope :next_month, -> { where("due_date > ? AND due_date < ?", Time.now.next_month.beginning_of_month, Time.now.next_month.end_of_month) }
+  scope :year_to_date, -> { where("due_date > ? AND due_date < ?", Time.now.beginning_of_year, Time.now) }
   scope :paid_commitments, -> { where(status: "Payé") }
   scope :unpaid_commitments, -> { where.not(status: "Payé") }
   scope :pending_invoice_commitmment, -> { where(status: "Facture en attente") }
